4a91741726e21c26011595b22ac8663dff47f584
rinnaaa2 <166714795+rinnaaa2@users.noreply.github.com>
2026-05-04 22:06:38 +0300

    callable в качестве допа

---
+++ lab5_dopzad/src/main/java/com/mycompany/lab5_dopzad/Lab5_dopzad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab5_dopzad;
+
+/**
+ *
+ * @author Марина
+ */
+public class Lab5_dopzad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
